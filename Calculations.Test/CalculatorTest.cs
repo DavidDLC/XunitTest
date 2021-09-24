309e4a0242e1c736b63a66510d077b63b04a9ec0
David Cruz <david.cruz@unosquare.com>
2021-09-24 17:00:49 -0500

    Asserting collections

--- Calculations.Test/CalculatorTest.cs
+++ Calculations.Test/CalculatorTest.cs
@@ -34,5 +34,40 @@ namespace Calculations.Test
             //asserts
             Assert.Equal(4.7, result,1);
         }
+
+        [Fact]
+        public void FiboDoesNotIncludeZero()
+        {
+            var cal = new Calculator();
+
+            Assert.All(cal.FiboNumbers, n => Assert.NotEqual(0, n)); // collections
+
+        }
+
+        [Fact]
+        public void FiboInclude13()
+        {
+            var cal = new Calculator();
+
+            Assert.Contains(13,cal.FiboNumbers); //collectins
+
+        }
+
+        [Fact]
+        public void FiboDoesNotInclude4()
+        {
+            var cal = new Calculator();
+            Assert.DoesNotContain(4, cal.FiboNumbers); //collections
+        }
+
+        [Fact]
+        public void CheckCollection()
+        {
+            var exprectedCollection = new List<int>() { 1, 1, 2, 3, 5, 8, 13 };
+            var cal = new Calculator();
+
+            Assert.Equal(exprectedCollection, cal.FiboNumbers);
+
+        }
     }
 }
--- ConsoleApp/Calculator.cs
+++ ConsoleApp/Calculator.cs
@@ -18,6 +18,8 @@ namespace Calculations
             return a + b;
         }
 
+        public List<int> FiboNumbers => new List<int> { 1, 1, 2, 3, 5, 8, 13 };
+
     }
 
 }
