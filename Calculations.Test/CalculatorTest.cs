fd186b39b5516550a6b79d08c1ee5ab6517b60bf
David Cruz <david.cruz@unosquare.com>
2021-09-24 16:06:20 -0500

    Create first test

---
+++ Calculations.Test/CalculatorTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Calculations.Test
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void testAdd()
+        {
+            Assert.True(true);
+        }
+    }
+}
