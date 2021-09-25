5f2b9c608832d33ee02dd0052b280246c0d15f33
David Cruz <david.cruz@unosquare.com>
2021-09-25 12:12:25 -0500

    TDD part2 create test, aceptance criteria and fail all test

---
+++ Calculations.Test/PasswordTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Calculations.Test
+{
+    public class PasswordTests
+    {
+        [Fact]
+        public void validate_GivenLongerThan8Chars_ReturnsTrue()
+        {
+            var testTarget = new DefaultPasswordValidator();
+            var password = MakeString(9);
+            var validationResult = testTarget.validate(password);
+
+            Assert.True(validationResult);
+        }
+
+        [Fact]
+        public void validate_GivenShorterThan8Chars_ReturnsFalse()
+        {
+            var testTarget = new DefaultPasswordValidator();
+            var password = MakeString(7);
+            var validationResult = testTarget.validate(password);
+
+            Assert.False(validationResult);
+        }
+
+        [Fact]
+        public void validate_GivenNoUpperCase_ReturnsFalse()
+        {
+            var testTarget = new DefaultPasswordValidator();
+            var password = "asd";
+            var validationResult = testTarget.validate(password);
+
+            Assert.False(validationResult);
+        }
+
+        [Fact]
+        public void validate_GivenUpperCase_ReturnsTrue()
+        {
+            var testTarget = new DefaultPasswordValidator();
+            var password = "aSd";
+            var validationResult = testTarget.validate(password);
+
+            Assert.True(validationResult);
+        }
+
+        private string MakeString(int length)
+        {
+            string result = string.Empty;
+            for (int i = 1; i < length; i++)
+            {
+                result += "a";
+            }
+
+            return result;
+        }
+    }
+}
